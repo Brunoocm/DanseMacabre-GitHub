ad4edd926578ffcfa4dee71fca83a4643c2d36f0
Eduardo Zawitoski <71409853+Zawitoski@users.noreply.github.com>
2021-08-31 16:13:01 -0300

    Pastas

---
+++ DanseMacabre/Assets/_DanseMacabre/Scripts/CharacterController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputHandler))]
+public class CharacterController : MonoBehaviour
+{
+    private InputHandler _input;
+
+    public bool teste;
+
+    [SerializeField]
+    private bool RotateTowardMouse;
+    [SerializeField]
+    private bool WalkWithCameraPosition;
+
+    [SerializeField]
+    private float MovementSpeed;
+    [SerializeField]
+    private float RotationSpeed;
+
+    [SerializeField]
+    private Camera Camera;
+
+    private Rigidbody rb;
+    public Vector3 targetVector;
+
+
+    private void Awake()
+    {
+        _input = GetComponent<InputHandler>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+        var movementVector = MoveTowardTarget(targetVector);
+
+        if (!RotateTowardMouse)
+        {
+            RotateTowardMovementVector(movementVector);
+        }
+        if (RotateTowardMouse)
+        {
+            RotateFromMouseVector();
+        }
+    }
+
+    private void RotateFromMouseVector()
+    {
+        Ray ray = Camera.ScreenPointToRay(_input.MousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        {
+            var target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+    }
+
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = MovementSpeed * Time.deltaTime;
+      
+        if(WalkWithCameraPosition)
+            targetVector = Quaternion.Euler(0, Camera.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
+
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
+        return targetVector;
+    }
+
+    private void RotateTowardMovementVector(Vector3 movementDirection)
+    {
+        if(movementDirection.magnitude == 0) { return; }
+        var rotation = Quaternion.LookRotation(movementDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, RotationSpeed);
+    }
+}
---
+++ DanseMacabre/Assets/_DanseMacabre/Scripts/InputHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public Vector2 InputVector { get; private set; }
+    public Vector3 MousePosition { get; private set; }
+    [HideInInspector]
+    public bool walking;
+
+    void Update()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+        InputVector = new Vector2(h, v);
+
+        MousePosition = Input.mousePosition;
+
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S))
+        {
+            walking = true;
+        }   
+        else walking = false;
+    }
+}
