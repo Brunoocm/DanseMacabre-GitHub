afc8ffb0ab2d3127f7da45faaa4a8cb2fc4fbd4a
Eduardo Zawitoski <71409853+Zawitoski@users.noreply.github.com>
2021-11-09 13:52:29 -0300

    Sonzinho

---
+++ DanseMacabre/Assets/_DanseMacabre/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource musicSource, effectsSource;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        effectsSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMasterVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+
+    public void ToggleEffects()
+    {
+        effectsSource.mute = !effectsSource.mute;
+    }
+    public void ToggleMusic()
+    {
+        musicSource.mute = !musicSource.mute;
+    }
+}
